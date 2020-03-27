3c8658dfc94989f5d5a8d45e8da6ff01d662fbea
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2020-03-26 23:04:06 -0400

    simulation.py: 1. Added R0 vs. time plot 2. Fixed bug where infected could get reinfected. 3. Fixed bug counting immunity so currently infected are included.
    WHERE_I_LEFT_OFF.txt :
    1. Added to repo

--- src/simulation.py
+++ src/simulation.py
@@ -23,6 +23,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from error import exit_with_error
 import random
+#random.seed(42)     # 
 from classes import AGENT
 
 def print_help(ExitCode):
@@ -77,38 +78,44 @@ def main():
     deathRate  = 0.01           # fraction of infected that die
     hostRate   = 0.20           # Fraction that get hospitalized
     healthyTime= 14             # Time after infection that person is healthy and no longer infectious
-    nDays      = 1000
-    sd         = 100              # Standard deviation for gaussian distribution for infection
+    nDays      = 100
+    sd         = 5              # Standard deviation for gaussian distribution for infection
+    N = 10**3       # Number of agents
 
 
     # Initialization
-    N = 10**4       # Number of agents
     agentL = []
     for i in range(N):
         agentL.append(AGENT())
 
     # Make one person infected
-    agentL[100].infected = True
-    agentL[100].start = 0
+    agentL[0].infected = True
+    agentL[0].start = 0
     yV = np.zeros([nDays])
+    R0V = np.zeros([nDays])     # Rate of infection as function of time
     xV = np.asarray(range(nDays))
 
     # Do simulation.  Run over 100 days. I'm sure I'm screwing up my monte-carlo methods
     for day in range(nDays):
         nInfect = 0
+        nTotal  = 0 # number infected + number immune
+        i = 0                               # Agent index
         for agent in agentL:
             if(agent.infected == True):
-                nInfect += 1
+                nInfect += 1                # Track number infected at this time step
                 diff = day - agent.start
                 if(diff > incubTime):
-                    idx = int(np.random.normal(loc=i, scale=sd))  #index of possible infection
+                    #idx = int(np.random.normal(loc=i, scale=sd))  #index of possible infection
+                    idx = random.randint(0,N-1)  #index of possible infection
                     # If random number outside of boundary, ignore
-                    if(idx < 0 or idx >=N):
+                    if(idx < 0 or idx >=N or idx == i):
                         continue
                     # prob of infection = R0 / (healthyTime - infectTime
                     prob = R0 / (healthyTime - infectTime)
                     rng = random.random()
-                    if(rng < prob and agentL[idx].immune == False):
+                    if(rng < prob and agentL[idx].immune == False and
+                       agentL[idx].infected == False
+                    ):
                         agentL[idx].infected= True
                         agentL[idx].start   = day
                         agent.nInfect += 1
@@ -116,15 +123,29 @@ def main():
                 if(diff > healthyTime):
                     agent.immune = True 
                     agent.infected = False
+                # Get R0 as function of time
+                if(agent.infected == True or agent.immune == True):
+                    R0V[day] += agent.nInfect
+                    nTotal += 1
+            i+=1
         yV[day] = nInfect
+        R0V[day] = R0V[day] / nTotal
 
     # Get number immune
     nImmune = 0
+    meanR0 = 0
+    # Visualize spatial distribution of infection
+    immuneV = np.zeros([N])
+    i=0
     for agent in agentL:
-        if(agent.immune == True):
+        if(agent.immune == True or agent.infected == True):
             nImmune += 1
-                
-    print("Number immune == {}".format(nImmune))
+            meanR0 += agent.nInfect
+            immuneV[i] = 1
+        i+=1
+    meanR0 = meanR0 / nImmune
+    print("Number immune == {}, R0 = {:.4f} ".format(nImmune,meanR0))
+
     
     fig, ax = plt.subplots(1,1)
     ylabel="Number of infections"
@@ -132,6 +153,11 @@ def main():
     ax.legend()
     plt.show()
 
+    fig, ax = plt.subplots(1,1)
+    ylabel="R0"
+    ax.plot(xV, R0V, label=ylabel)
+    ax.legend()
+    plt.show()
 
 
     print("Ended : %s"%(time.strftime("%D:%H:%M:%S")))
