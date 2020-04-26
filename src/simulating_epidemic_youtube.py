eec0b7e54aa66ea6d99b1747d3dfa2bc2549ca35
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2020-04-25 23:25:45 -0400

    Added quarentine option to code (untested). Changes the following :
    simulating_epidemic_youtube.py :
    1. print_help() : modified to reflect additional quarantine option
    2. displacement() : permit passing either AGENT object or a list of [x,y]
    3. move_agent() :
       a) Added a bunch of code to try and prevent susceptibles from entering the radius of
          an infected Agent. It is a pain to code, and I only consider the case of the first
          infected agent encountered. If I consider multiple infected agents, it gets logically
          complicated to code for probably little benefit.
       b) I also don't consider the case where a susceptible avoids an infected and then goes out of
          bounds.
    4. main():
       a) Added additional code to label AGENTS as quarentined or not

--- src/simulating_epidemic_youtube.py
+++ src/simulating_epidemic_youtube.py
@@ -23,6 +23,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.patches import Circle
 from error import exit_with_error
+from scipy import optimize
 import random
 random.seed(42)     # Change later
 from classes import AGENT
@@ -37,10 +38,15 @@ def print_help(ExitCode):
     FUTURE:
     """
     sys.stderr.write(
-        "python3 ./src/osu_ide_replication_attempt.py \n"
+        "python3 ./src/osu_ide_replication_attempt.py [quarentine]\n"
+        "   [quarentine] : optional, puts infected in quarentine 2 days after symptoms  \n"
+        "      \n"
+        "      \n"
+        "   After running, create a movie via : "
+        "       ffmpeg -framerate 4 -pattern_type glob -i 'tmp/*.png' -c:v libx264 out.mp4\n"
         "      \n"
         "   To Run: \n"
-        "   source ~/.local/virtualenvs/python3.7/bin/activate\n")
+        "       source ~/.local/virtualenvs/python3.7/bin/activate\n")
     sys.exit(ExitCode)
 
 
@@ -53,17 +59,29 @@ def displacement(Agent1=None, Agent2=None):
     DEBUG:
     FUTURE:
     """
-    x1=Agent1.posL[0]
-    y1=Agent1.posL[1]
-
-    x2=Agent2.posL[0]
-    y2=Agent2.posL[1]
+    if(type(Agent1) == AGENT):
+        x1=Agent1.posL[0]
+        y1=Agent1.posL[1]
+    elif(type(Agent1) == list):
+        x1=Agent1[0]
+        y1=Agent1[1]
+    else:
+        exit_with_error("ERROR!!! {} not a supported type\n".format(type(Agent1)))
+        
+    if(type(Agent2) == AGENT):
+        x2=Agent2.posL[0]
+        y2=Agent2.posL[1]
+    elif(type(Agent2) == list):
+        x2=Agent2[0]
+        y2=Agent2[1]
+    else:
+        exit_with_error("ERROR!!! {} not a supported type\n".format(type(Agent2)))
 
     return np.sqrt( (x1-x2)**2 + (y1-y2)**2)
 
 
 
-def move_agent(Agent=None, DeltaT=None):
+def move_agent(Agent=None, AgentL=None, InfectDist=None, Quarentine=None, DeltaT=None):
     """
     ARGS:
     RETURN:
@@ -72,31 +90,114 @@ def move_agent(Agent=None, DeltaT=None):
     DEBUG:
     FUTURE:
     """
-    x = Agent.vL[0] * DeltaT + Agent.posL[0]
-    y = Agent.vL[1] * DeltaT + Agent.posL[1]
+    xi= Agent.posL[0]
+    yi= Agent.posL[1]
+    vx= Agent.vL[0]
+    vy= Agent.vL[1]
+    v = np.sqrt(vx**2 + vy**2)
+    xf= Agent.vL[0] * DeltaT + Agent.posL[0]
+    yf= Agent.vL[1] * DeltaT + Agent.posL[1]
+    r = InfectDist      # Radius about quarantined individual
 
-    if(x < 0):
-        x = -1.0 * x
-        Agent.vL[0] = -1.0 * Agent.vL[0]
-    if(y < 0):
-        y = -1.0 * y
-        Agent.vL[1] = -1.0 * Agent.vL[1]
-    if(x > 1.0):
-        d = x - 1.0
-        x = x - d
-        Agent.vL[0] = -1.0 * Agent.vL[0]
-    if(y > 1.0):
-        d = y - 1.0
-        y = y - d
-        Agent.vL[1] = -1.0 * Agent.vL[1]
-    # Adjust Position
-    Agent.posL[0] = x
-    Agent.posL[1] = y
-    # Adjust velocity
-    dvx = random.uniform(-1,1)/100.0 # Want crossing time to be about 25 steps
-    dvy = random.uniform(-1,1)/100.0
-    Agent.posL[0] += dvx
-    Agent.posL[1] += dvy
+    # Check if quarentined agent nearby.
+    #   1. Only consider 1st quarantine encountered b/c it could be potentially very
+    #      challenging to solve for preventing a susceptible from completely avoiding _all_
+    #      quarantined agents.
+    #   2. 
+    #   3. 
+    #
+    if(Quarentine == True):
+        avoidInfect = False
+        while(avoidInfect == False):
+            # displacement, Agent final - initial
+            dfi = np.sqrt((xf-xi)**2 + (yf-yi)**2)
+            # Get line function,     y = mx + b
+            m   = (yf-yi)/(xf-xi)  # Slope of line
+            b   = yf - m*xf        # Pick a point on the line, solve for intercept
+
+            for agent in AgentL:
+                # Must be quarentined to avoid
+                if(agent.quarentine == False):
+                    continue
+                xc = agent.posL[0]
+                yc = agent.posL[1]
+                # displ, quarntined - Agent initial
+                dfq = np.sqrt((xc-xf)**2 + (yc-yf)**2)
+                # displ, quarntined - Agent final 
+                diq = np.sqrt((xc-xi)**2 + (yc-yi)**2)
+                # There might be a collision.
+                if(dfq <= r + dfi or diq <= r + dfi):
+                    # Get circle of exclusion line, recall 
+                    #   0 = (x - xc)^2 + (y - yc)^2 - r^2
+                    #   xc,yc = x,yposition of center of circle
+                    def f(x):
+                        y = m*x+b
+                        return( (x-xc)**2 + (y-yc)**2 - r**2)
+                    # Is there a root or crossing?  f(a) * f(b) = -1
+                    if( f(xc-r) * f(xc+r) < 0):
+                        xroots = optimize.fsolve(f, [xc-r, xc+r])
+                        # If there are two roots, which do i pick? Pick closest to Agent
+                        if(len(xroots) == 2):
+                            x1=xroots[0]
+                            y1=f(x1)
+                            d1=displacement(Agent,[x1,y1])
+                            x2=xroots[1]
+                            y2=f(x2)
+                            d2=displacement(Agent,[x2,y2])
+                            # Use 1st root b/c it is closer
+                            if(d1<d2):
+                                x=x1
+                                y=y1
+                            else:
+                                x=x2
+                                y=y2
+                                
+                        elif(len(xroots) == 1):
+                            x = xroot
+                            y =f(x)
+                        else:
+                            exit_with_error("ERROR!!! I don't understand how there can "
+                                            "be more than 2 roots!\n")
+                        rvect = [x-xc, y-yc]
+                        if(np.isclose(np.sqrt(rvect*rvect), r) == False):
+                            exit_with_error("ERROR!!! I don't know how |rvect| != |r|\n")
+
+                        # Now get angle between rvector and velocity vector
+                        theta = np.arccos( (vx*rvect[0] + vy*rvect[1]) /
+                                           ((vx**2 + vy**2)*(rvect[0]**2 + rvect[1]**2)))
+                        # Angle of reflection w/r/t to the tangent line on circle 
+                        phi = np.pi - theta
+                        vx = v * np.sin(phi)
+                        vy = v * np.cos(phi)
+                        xf = vx * DeltaT + xi
+                        yf = vy * DeltaT + yi
+                        break
+                    else: 
+                        continue
+            avoidInfect = True
+    else:
+        if(xf < 0):
+            xf = -1.0 * xf
+            Agent.vL[0] = -1.0 * Agent.vL[0]
+        if(yf < 0):
+            yf = -1.0 * yf
+            Agent.vL[1] = -1.0 * Agent.vL[1]
+        if(xf > 1.0):
+            d = xf - 1.0
+            xf = xf - d
+            Agent.vL[0] = -1.0 * Agent.vL[0]
+        if(yf > 1.0):
+            d = yf - 1.0
+            yf= yf - d
+            Agent.vL[1] = -1.0 * Agent.vL[1]
+        # Adjust Position
+        Agent.posL[0] = xf
+        Agent.posL[1] = yf
+        # Adjust velocity
+        dvx = random.uniform(-1,1)/100.0 # Want crossing time to be about 25 steps
+        dvy = random.uniform(-1,1)/100.0
+        Agent.posL[0] += dvx
+        Agent.posL[1] += dvy
     
 
 def main():
@@ -119,8 +220,12 @@ def main():
     # Get options 
     if(len(sys.argv) > 1 and "-h" in sys.argv[1]):
         print_help(0)
-    elif(nArg != 1):
+    elif(nArg != 1 and nArg != 2):
         print_help(1)
+    elif(nArg == 1):
+        quarantine = False
+    elif(nArg == 2 and sys.argv[1] == "quarentine"):
+        quarantine = True
 
     startTime = time.time()
     print("{} \n".format(sys.argv),flush=True)
@@ -157,11 +262,13 @@ def main():
         iyL = []
         rxL = []    # Removed xL
         ryL = []
+        ### Only if quarentining infected individuals
+        if(quarentine == True):
+            qxL = []    # quarentine
+            qyL = []
 
         for i in range(len(agentL)):
             agent = agentL[i]
-            # Move
-            move_agent(agent,dt)
             # Generate for plot
             #xL.append(agent.posL[0])
             #yL.append(agent.posL[1])
@@ -177,6 +284,18 @@ def main():
             if(agent.immune == True):
                 rxL.append(agent.posL[0])
                 ryL.append(agent.posL[1])
+
+            ### Only if quarentining infected individuals
+            if(quarentine == True):
+                if((step - agent.start - asymptomaticTime >= 2)):
+                    agent.quarentine = True
+                if(agent.quarentine == True):
+                    qxL.append(agent.posL[0])
+                    qyL.append(agent.posL[1])
+                    continue
+
+            # Move
+            move_agent(agent,agentL,dt)
 
             # Susceptible Group - Check if infected
             if(agent.infected == False or (step - agent.start < asymptomaticTime)):
